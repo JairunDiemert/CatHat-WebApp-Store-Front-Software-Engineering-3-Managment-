ad4a9613d7a6bddcc5f4e7a86c3d73d36f252729
Leesa Marie Price <literaryleesa@gmail.com>
2020-09-04 13:18:10 -0500

    added routing for catalog page

--- cat-hats/src/app/app-routing.module.ts
+++ cat-hats/src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CatalogPageComponent } from './pages/catalog-page/catalog-page.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'catalog', component: CatalogPageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
