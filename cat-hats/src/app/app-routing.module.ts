04ab8ecbed719fc4e617c188937bec13e93e5a72
JairunDiemert <jsd25@students.uwf.edu>
2020-09-04 21:41:08 -0700

    home-page component added

--- cat-hats/src/app/app-routing.module.ts
+++ cat-hats/src/app/app-routing.module.ts
@@ -1,14 +1,15 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import { CatalogPageComponent } from './pages/catalog-page/catalog-page.component';
-
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { CatalogPageComponent } from "./pages/catalog-page/catalog-page.component";
+import { HomePageComponent } from "./pages/home-page/home-page.component";
 
 const routes: Routes = [
-  {path: 'catalog', component: CatalogPageComponent}
+  { path: "catalog", component: CatalogPageComponent },
+  { path: "", component: HomePageComponent },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- cat-hats/src/app/app.module.ts
+++ cat-hats/src/app/app.module.ts
@@ -1,22 +1,21 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { CatalogPageComponent } from './pages/catalog-page/catalog-page.component';
-import { ItemCardComponent } from './modules/item-card/item-card.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { CatalogPageComponent } from "./pages/catalog-page/catalog-page.component";
+import { ItemCardComponent } from "./modules/item-card/item-card.component";
+import { HomePageComponent } from "./pages/home-page/home-page.component";
 
 @NgModule({
   declarations: [
     AppComponent,
     CatalogPageComponent,
-    ItemCardComponent
+    ItemCardComponent,
+    HomePageComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
