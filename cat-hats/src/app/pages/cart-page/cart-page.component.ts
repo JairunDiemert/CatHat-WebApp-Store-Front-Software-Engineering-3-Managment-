977b326f5cc120c147746805c79019bbd2c8c5a8
yngo27 <yn15@students.uwf.edu>
2020-09-05 00:19:40 -0700

    added shopping cart components

---
+++ cat-hats/src/app/pages/cart-page/cart-page.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { Cart } from '../../models/Cart';
+
+@Component({
+  selector: 'app-cart-page',
+  templateUrl: './cart-page.component.html',
+  styleUrls: ['./cart-page.component.css']
+})
+export class CartPageComponent implements OnInit {
+ 
+  //array of cart items
+  cart_Item:Cart[];
+
+  constructor() { }
+
+  ngOnInit(): void {
+
+    //TO DO: listing data for each cart item from database
+    this.cart_Item = [
+      {
+        title: 'Party Cat Hat'
+      }
+    ]
+  } 
+  
+}
