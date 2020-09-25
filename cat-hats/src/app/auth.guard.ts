0ec31e9387ef281295be11cbd3f5fe1349fa5fe1
JairunDiemert <jsd25@students.uwf.edu>
2020-09-25 15:14:49 -0700

    set up authentication of persistant cookie after sign on.

--- cat-hats/src/app/auth.guard.ts
+++ cat-hats/src/app/auth.guard.ts
@@ -7,13 +7,19 @@ import {
 } from "@angular/router";
 import { Observable } from "rxjs";
 import { AuthService } from "./auth.service";
+import { UserService } from "./user.service";
 import { Router } from "@angular/router";
+import { map } from "rxjs/operators";
 
 @Injectable({
   providedIn: "root",
 })
 export class AuthGuard implements CanActivate {
-  constructor(private auth: AuthService, private router: Router) {}
+  constructor(
+    private auth: AuthService,
+    private router: Router,
+    private user: UserService
+  ) {}
 
   canActivate(
     route: ActivatedRouteSnapshot,
@@ -23,9 +29,19 @@ export class AuthGuard implements CanActivate {
     | Promise<boolean | UrlTree>
     | boolean
     | UrlTree {
-    if (!this.auth.isLoggedIn) {
-      this.router.navigate(["login"]);
+    if (this.auth.isLoggedIn) {
+      return true;
     }
-    return this.auth.isLoggedIn;
+    return this.user.isLoggedIn().pipe(
+      map((res) => {
+        if (res.status) {
+          this.auth.setLoggedIn(true);
+          return true;
+        } else {
+          this.router.navigate(["login"]);
+        }
+        return false;
+      })
+    );
   }
 }
--- cat-hats/src/app/user.service.ts
+++ cat-hats/src/app/user.service.ts
@@ -1,11 +1,15 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
+import { Observable } from "rxjs";
 
 interface myData {
   message: string;
   success: boolean;
 }
 
+interface isLoggedIn {
+  status: boolean;
+}
 @Injectable({
   providedIn: "root",
 })
@@ -14,5 +18,9 @@ export class UserService {
 
   getSomeData() {
     return this.http.get<myData>("/api/database.php");
+  }
+
+  isLoggedIn(): Observable<isLoggedIn> {
+    return this.http.get<isLoggedIn>("/api/isloggedin.php");
   }
 }
