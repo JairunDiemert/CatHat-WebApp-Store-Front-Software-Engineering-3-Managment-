8db1228f58b6d23a5b87a0e50b8833d5bc120810
JairunDiemert <jsd25@students.uwf.edu>
2020-09-25 10:14:24 -0700

    Log in is now working with proxy server and validation.

--- cat-hats/src/app/auth.service.ts
+++ cat-hats/src/app/auth.service.ts
@@ -1,12 +1,17 @@
 import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
 
 @Injectable({
   providedIn: "root",
 })
 export class AuthService {
-  constructor() {}
+  constructor(private http: HttpClient) {}
 
-  getUserDetails() {
+  getUserDetails(username, password) {
     ///post these details to API server return user info if correct
+    return this.http.post<any>("/api/auth.php", {
+      username,
+      password,
+    });
   }
 }
--- cat-hats/src/app/login/login.component.ts
+++ cat-hats/src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from "@angular/core";
+import { AuthService } from "../auth.service";
 
 @Component({
   selector: "app-login",
@@ -6,12 +7,23 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./login.component.css"],
 })
 export class LoginComponent implements OnInit {
-  constructor() {}
+  constructor(private Auth: AuthService) {}
 
-  ngOnInit(): void {}
+  ngOnInit() {}
 
   loginUser(event) {
     event.preventDefault();
-    console.log(event);
+    const target = event.target;
+    const username = target.querySelector("#username").value;
+    const password = target.querySelector("#password").value;
+
+    this.Auth.getUserDetails(username, password).subscribe((data) => {
+      if (data.success) {
+        // redirect the person to /admin
+      } else {
+        window.alert(data.message);
+      }
+    });
+    console.log(username, password);
   }
 }
