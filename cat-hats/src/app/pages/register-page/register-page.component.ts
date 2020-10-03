e57137a779507241d14a61f62351f8ffb439348f
yngo27 <yn15@students.uwf.edu>
2020-10-03 06:53:39 -0700

    Remove register card; update register ngform info handling

--- cat-hats/src/app/pages/register-page/register-page.component.ts
+++ cat-hats/src/app/pages/register-page/register-page.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { NgModel } from '@angular/forms';
+import { customerModel } from 'src/app/models/customer-model';
 
 @Component({
   selector: 'app-register-page',
@@ -9,7 +12,42 @@ export class RegisterPageComponent implements OnInit {
 
   constructor() { }
 
+  public customer: customerModel = {
+    username: "",
+    cartItems: [],
+    cartTotal: 0,
+    name: "",
+    email: "",
+    address: "",
+    password: ""
+  };
+
   ngOnInit(): void {
+    this.resetForm();
   }
 
+  onSubmit(registerForm: NgForm) {
+    this.customer.username = registerForm.value.username;
+    this.customer.name = registerForm.value.name;
+    this.customer.email = registerForm.value.email;
+    this.customer.address = registerForm.value.address;
+    this.customer.password = registerForm.value.password;
+  }
+
+
+  resetForm(form?:NgModel)
+  {
+    if(form != null) {
+      form.reset();
+      this.customer = {
+        username: "",
+        cartItems: [],
+        cartTotal: 0,
+        name: "",
+        email: "",
+        address: "",
+        password: ""
+      }
+    }
+  }
 }
