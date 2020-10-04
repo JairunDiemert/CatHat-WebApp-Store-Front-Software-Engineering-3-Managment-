c70e1fef6291ef189dd326a70159c86d4bd8bbdd
Leesa Marie Price <literaryleesa@gmail.com>
2020-10-04 16:43:26 -0500

    login-card now can log you in

--- cat-hats/src/app/login/login.component.ts
+++ cat-hats/src/app/login/login.component.ts
@@ -10,7 +10,7 @@ import { Router } from "@angular/router";
 export class LoginComponent implements OnInit {
   constructor(private Auth: AuthService, private router: Router) {}
 
-  ngOnInit() {}
+  ngOnInit() {} 
 
   loginUser(event) {
     event.preventDefault();
--- cat-hats/src/app/modules/login-card/login-card.component.ts
+++ cat-hats/src/app/modules/login-card/login-card.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/auth.service';
 
 @Component({
   selector: 'app-login-card',
@@ -7,9 +10,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginCardComponent implements OnInit {
 
-  constructor() { }
+  constructor(private Auth: AuthService, private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  loginShow : Boolean = true;
+
+  onSubmit(login: NgForm){
+    const username = login.value.email;
+    const password = login.value.password;
+
+    this.Auth.getUserDetails(username, password).subscribe((data) => {
+      if (data.success) {
+        this.router.navigate(["/"]);
+        this.Auth.setLoggedIn(true);
+        this.loginShow = false;
+      } else {
+        window.alert(data.message);
+        this.loginShow = true;
+      }
+    });
+    console.log(username, password);
   }
 
 }
