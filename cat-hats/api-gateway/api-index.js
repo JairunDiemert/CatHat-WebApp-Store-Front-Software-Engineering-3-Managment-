cc182b9566908312bcf584b3e39e85f85b3a2586
yngo27 <yn15@students.uwf.edu>
2020-10-07 09:04:04 -0700

    Added api-gateway folder and api connective functionality

---
+++ cat-hats/api-gateway/api-index.js
@@ -0,0 +1,30 @@
+const axios = require('axios');
+const bodyParser = require("body-parser");
+const app = require('express')();
+app.use(bodyParser.json());
+//angular post connects to this endpoint, mirrors the post in the user service
+app.post("/api/login", async (req, res) => {
+
+    //axios to make http request to user service
+    axios.post('http://localhost:12345/api/login', {
+
+        //retrieve variable info from json payload that came from client side, sends to user service
+        email: req.body.email,
+        password: req.body.password   
+    })
+    //axiosResponse: json payload returned from user service
+    .then(axiosResponse => {
+        console.log(axiosResponse);
+        //sends json request back to calling client
+        res.json(axiosResponse.data);
+    })
+    //catch calls errors with sending or receiving request
+    .catch(axiosError => {
+        console.log(axiosError);
+    });
+});
+
+
+
+app.listen(54321);
+
