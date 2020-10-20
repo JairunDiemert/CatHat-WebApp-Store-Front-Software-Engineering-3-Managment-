039240855cfeede07e51bd1748774272a5696ccf
Leesa Marie Price <literaryleesa@gmail.com>
2020-10-20 15:11:45 -0500

    catalog now shows items from db

--- cat-hats/src/app/pages/catalog-page/catalog-page.component.ts
+++ cat-hats/src/app/pages/catalog-page/catalog-page.component.ts
@@ -14,30 +14,26 @@ export class CatalogPageComponent implements OnInit {
 
   constructor(private Catalog: CatalogService) {
     this.Catalog.getCatalog().subscribe((data) => {
-      console.log(data);
       if (data.success) {
-        alert("The catalog has been retrieved from the database.");
+        this.list = data.data;
+        let initialList = [];
+        this.list.forEach(item => {
+          initialList.push({
+            itemName: item.title,
+            itemPrice: (item.price).toFixed(2),
+            itemID: item.id,
+            itemDescription: item.description,
+            itemQuantity: item.quantity,
+            itemImg : item.img
+          });
+        })
+        this.pageItems = initialList;
       } else {
         alert("The catalog has not been successfully retrieved from the database.");
       }
-
-      this.list = data;
     });
   }
 
   ngOnInit(): void {
-    let list = [];
-    for (let i = 0; i < 20; i++) {
-      list.push({
-        itemName: "Small Cat Hat",
-        itemPrice: 15.99,
-        itemID: i + 1,
-        itemDescription: "A small hat for a cat.",
-        itemQuantity: 15,
-        itemImg: "./../../../assets/Hat_200x200.png",
-      });
-    }
-    this.pageItems = list;
-    console.log(list);
   }
 }
