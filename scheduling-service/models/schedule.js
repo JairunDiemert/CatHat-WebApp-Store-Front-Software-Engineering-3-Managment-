2830a070104b75f9acf31d889b8e639ab89c05c3
yngo27 <yn15@students.uwf.edu>
2020-11-12 09:08:07 -0800

    Update schedule schema to contain shopping cart

--- scheduling-service/models/schedule.js
+++ scheduling-service/models/schedule.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const ScheduleSchema = new mongoose.Schema({
   scheduleDate: { type: String, required: true }, //need to be changed to Date type after testing
   userEmail: { type: String, required: true },
-  catalogTitle: { type: String, required: true },
+  shippingCart: [mongoose.Schema.Types.Mixed]
 });
 
 const Schedule = mongoose.model("Schedule", ScheduleSchema);
